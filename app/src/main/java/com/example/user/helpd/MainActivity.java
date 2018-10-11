1087c81b56c39b11013cb5cc585fd3cc8ff24200
azycray <500060253@stu.upes.ac.in>
2018-10-12 01:29:35 +0530

    Added Left nav drawer with basic functionality and UI

--- app/src/main/java/com/example/user/helpd/MainActivity.java
+++ app/src/main/java/com/example/user/helpd/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.user.helpd;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /***** applying toolbar as the app bar  and setting the hamburger icon *****/
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        /*************************************************************************/
+
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView= findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                drawerLayout.closeDrawers();
+                //enter code for what to do when item selected
+                //use if else here according to item selected
+
+                return true;
+            }
+        });
+
+
+    }
+
+    /***** Functionality for what to do when menu icon selected *****/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START); //opens drawer
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
